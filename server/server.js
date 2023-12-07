fc86f6868b91c7e0429e8cd8cee8f27493e3db92
BadarauDavid <badaraudavidiulian@gmail.com>
2023-12-07 14:47:46 +0200

    connect wtih mongo DB and add To fav

--- server/server.js
+++ server/server.js
@@ -1,15 +1,46 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+let Movie = require("./model/Movie.js");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use((req,res,next)=>{
-    console.log(`Time: `, Date.now());
-    next();
-})
+app.use(function (req, res, next) {
+  res.header("Acces-Control-Allow-Origin", "*");
+  res.header(
+    "Acces-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.urlencoded({ extended: false }));
 
+mongoose.connect(`mongodb://127.0.0.1:27017/CinematePick`);
 
-app.get('/',(req,res)=>{
-    res.send('Succesful response.');
+app.get("/", (req, res) => {
+  res.send("Succesful response.");
 });
 
-app.listen(3000, () => console.log('Exemple app is listening on port 3000.'));
+app.post("/api/addToFav", (req, res) => {
+  const name = req.body.name;
+  const year = req.body.year;
+  const status = req.body.status;
+
+  const movie = new Movie({
+    name,
+    year,
+    status,
+  });
+
+  try {
+    movie.save();
+    res.status(200).json({ succes: true });
+  } catch (err) {
+    res.status(404).json({ succes: false });
+  }
+});
+
+app.listen(8000, () => console.log("Exemple app is listening on port 8000."));
