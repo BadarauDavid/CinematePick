be626bb9e00902ddde54ad935b9157867cadbb91
BadarauDavid <badaraudavidiulian@gmail.com>
2023-12-11 15:16:23 +0200

    Revert "carousel first page"
    This reverts commit 1e4ffcdd36ec7398acbcbbb2e73a65c09fc0cdc4.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,14 @@
 // /** @type {import('tailwindcss').Config} */
 
 const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports =withMT({
-  content: ["./src/**/*.{js,jsx,ts,tsx}",
-  'node_modules/flowbite-react/lib/esm/**/*.js'],
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-],
-  darkMode:`class`,
-})
-
+  plugins: [require("flowbite/plugin")],
+  darkMode: `class`,
+});
